4dd1f957be1ffba3d9579f83518c81df171256eb
Bideo Wego <bideowego@gmail.com>
2017-03-31 15:21:51 -0400

    Update screenshot module

--- controllers/screenshots_controller.js
+++ controllers/screenshots_controller.js
@@ -3,42 +3,55 @@ var validator = require('validator');
 
 // ----------------------------------------
 // ScreenshotsController
+// ---------------------------------------- 
+
+// ----------------------------------------
+// Private
 // ----------------------------------------
 
-ScreenshotsController = {
-  make: function(request, response) {
-    response.render('layouts/application', {
-      view: 'screenshots/form',
-      flash: {
-        danger: request.flash('error')
-      },
-      csrfToken: process.env.CSRF_TOKEN
-    });
-  },
-
-
-  create: function(request, response) {
-    if (!validator.isURL(request.body.url)) {
-      request.flash('error', 'Not a URL');
-      response.redirect('/');
-    } else {
-      screenshot.config(request.body);
-      
-      screenshot(function(data) {
-        if (request.body.format === 'base64') {
-          response.write(data);
-        } else if (request.body.format === 'image_src') {
-          response.write('data:image/jpeg;base64,' + data);
-        } else {
-          var image = new Buffer(data, 'base64');
-          response.writeHead(200, { 'Content-Type': 'image/jpeg' });
-          response.write(image);
-        }
-        response.end();
-      }, function(error) {
-        response.json({ error: error });
-      });
+var _createScreenshot = function(request, response) {
+  screenshot.config(request.body);
+  screenshot(function(data) {
+    var formattedData = {
+      "base64": data,
+      "image_src": 'data:image/jpeg;base64,' + data
+    }[request.body.format];
+    if (!formattedData) {
+      formattedData = new Buffer(data, 'base64');
+      response.writeHead(200, { 'Content-Type': 'image/jpeg' });
     }
+    response.write(formattedData);
+    response.end();
+  }, function(error) {
+    response.json({ error: error });
+  });
+};
+
+
+// ----------------------------------------
+// Public
+// ----------------------------------------
+
+var ScreenshotsController = {};
+
+
+ScreenshotsController.make = function(request, response) {
+  response.render('layouts/application', {
+    view: 'screenshots/form',
+    flash: {
+      danger: request.flash('error')
+    },
+    csrfToken: process.env.CSRF_TOKEN
+  });
+};
+
+
+ScreenshotsController.create = function(request, response) {
+  if (!validator.isURL(request.body.url)) {
+    request.flash('error', 'Not a URL');
+    response.redirect('/');
+  } else {
+    _createScreenshot(request, response);
   }
 };
 
