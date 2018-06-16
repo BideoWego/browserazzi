4123f95da9cbda88e40bded44d048112c92f7850
Bideo Wego <bideowego@gmail.com>
2018-06-15 22:55:43 -0400

    Remove old files and generate new project structure

--- app.js
+++ app.js
@@ -1,12 +1,157 @@
-// ----------------------------------------
-// App
-// ----------------------------------------
+const express = require('express');
+const app = express();
 
-var app = require('./config/application')();
-var environment = require('./config/environment')(app);
-var routes = require('./config/routes')(app);
-var errors = require('./config/errors')(app);
-var app = require('./config/server')(app);
+
+// ----------------------------------------
+// App Variables
+// ----------------------------------------
+app.locals.appName = 'My App';
+
+
+// ----------------------------------------
+// ENV
+// ----------------------------------------
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+
+// ----------------------------------------
+// Body Parser
+// ----------------------------------------
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+// ----------------------------------------
+// Sessions/Cookies
+// ----------------------------------------
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
+
+app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+  keys: [
+    process.env.SESSION_SECRET || 'secret'
+  ]
+}));
+
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
+
+
+// ----------------------------------------
+// Flash Messages
+// ----------------------------------------
+const flash = require('express-flash-messages');
+app.use(flash());
+
+
+// ----------------------------------------
+// Method Override
+// ----------------------------------------
+const methodOverride = require('method-override');
+const getPostSupport = require('express-method-override-get-post-support');
+
+app.use(methodOverride(
+  getPostSupport.callback,
+  getPostSupport.options // { methods: ['POST', 'GET'] }
+));
+
+
+// ----------------------------------------
+// Referrer
+// ----------------------------------------
+app.use((req, res, next) => {
+  req.session.backUrl = req.header('Referer') || '/';
+  next();
+});
+
+
+// ----------------------------------------
+// Public
+// ----------------------------------------
+app.use(express.static(`${__dirname}/public`));
+
+
+// ----------------------------------------
+// Logging
+// ----------------------------------------
+const morgan = require('morgan');
+const morganToolkit = require('morgan-toolkit')(morgan);
+
+app.use(morganToolkit());
+
+
+// ----------------------------------------
+// Routes
+// ----------------------------------------
+app.get('/', (req, res) => {
+  req.flash('Hi!');
+  res.render('welcome/index');
+});
+
+
+// ----------------------------------------
+// Template Engine
+// ----------------------------------------
+const expressHandlebars = require('express-handlebars');
+const helpers = require('./helpers');
+
+const hbs = expressHandlebars.create({
+  helpers: helpers,
+  partialsDir: 'views/',
+  defaultLayout: 'application'
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+// ----------------------------------------
+// Server
+// ----------------------------------------
+const port = process.env.PORT ||
+  process.argv[2] ||
+  3000;
+const host = 'localhost';
+
+const args = process.env.NODE_ENV === 'production' ?
+  [port] :
+  [port, host];
+
+args.push(() => {
+  console.log(`Listening: http://${ host }:${ port }\n`);
+});
+
+if (require.main === module) {
+  app.listen.apply(app, args);
+}
+
+
+// ----------------------------------------
+// Error Handling
+// ----------------------------------------
+app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  if (err.stack) {
+    err = err.stack;
+  }
+  res.status(500).render('errors/500', { error: err });
+});
+
 
 module.exports = app;
+
+
+
+
+
 
